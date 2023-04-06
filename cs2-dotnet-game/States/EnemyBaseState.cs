fd42a82b2b37deecd93ff382ed68cea8dc44c2f4
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2023-04-06 12:52:10 +0200

    fight "animations"

--- cs2-dotnet-game/States/EnemyBaseState.cs
+++ cs2-dotnet-game/States/EnemyBaseState.cs
@@ -29,7 +29,7 @@ public class EnemyBaseState : State
 
     private Texture2D backgroundTexture;
     private Texture2D playerTexture;
-    private readonly Texture2D enemyTexture;
+    private Texture2D enemyTexture;
 
     private GameManager _gm;
 
@@ -52,12 +52,16 @@ public class EnemyBaseState : State
         _gm = gm;
 
         buttonAttack.OnClick += Attack;
+        buttonDefend.OnClick += Defend;
+        buttonInventory.OnClick += Inventory;
+        buttonSkipRound.OnClick += SkipRound;
+        
     }
     public override void Draw(GameManager gm)
     {
         Globals.SpriteBatch.Draw(backgroundTexture, new Rectangle(0, 0, 1920, 1080), Color.White);
-        Globals.SpriteBatch.Draw(playerTexture, new Rectangle(100, 250, 500, 500), Color.White);
-        Globals.SpriteBatch.Draw(enemyTexture, new Rectangle(1000, 250, 740, 461), Color.White);
+        Globals.SpriteBatch.Draw(playerTexture, new Rectangle(200, 300, 500, 500), Color.White);
+        Globals.SpriteBatch.Draw(enemyTexture, new Rectangle(1100, 300, 740, 500), Color.White);
         buttonLeave.Draw();
         buttonAttack.Draw();
         buttonDefend.Draw();
@@ -87,33 +91,41 @@ public class EnemyBaseState : State
     {
         UpdateCombat(1);
     }
+    private void AttackSpell(object sender, EventArgs e)
+    {
+        UpdateCombat(2);
+    }
 
     private void Defend(object sender, EventArgs e)
     {
         //defend against the enemy
+        UpdateCombat(3);
     }
 
     private void SkipRound(object sender, EventArgs e)
     {
         //skip the round
+        UpdateCombat(4);
     }
 
     private void Inventory(object sender, EventArgs e)
     {
         //open the inventory
+        UpdateCombat(7);
     }
 
-    private void Leave(object sender, EventArgs e)
+    private async void Leave(object sender, EventArgs e)
     {
         //reduce player ex and leave the battle state
         xp -= 100;
+        await Task.Delay(300);
         _gm.ChangeState(GameStates.Menu);
 
     }
     //fight state 0 = player idle, 1 = player attack, 2 = player attack spell, 3 = player defend 4 = player skip round, 5 = player inventory, 6 = enemy idle, 7 = enemy attack, 8 = enemy defeat.
     private async void UpdateCombat(int fightState)
     {
-        await Task.Delay(1000);
+        await Task.Delay(300);
         
         switch (fightState)
         {
@@ -123,6 +135,7 @@ public class EnemyBaseState : State
             case 1:
                 //player attack
                 playerTexture = Globals.Content.Load<Texture2D>("Player/playerShooting");
+                enemyTexture = Globals.Content.Load<Texture2D>("Enemy/enemyDefend");
                 break;
             case 2:
                 //player attack spell
@@ -141,14 +154,20 @@ public class EnemyBaseState : State
                 break;
             case 6:
                 //enemy idle
+                enemyTexture = Globals.Content.Load<Texture2D>("Enemy/enemy1");
                 break;
             case 7:
                 //enemy attack
+                enemyTexture = Globals.Content.Load<Texture2D>("Enemy/enemyFight");
+                playerTexture = Globals.Content.Load<Texture2D>("Player/playerDefend");
                 break;
             case 8:
                 //enemy defeat
+                enemyTexture = Globals.Content.Load<Texture2D>("Enemy/enemyDead");
                 break;
             default:
+                playerTexture = Globals.Content.Load<Texture2D>("Player/playerIdle");
+                enemyTexture = Globals.Content.Load<Texture2D>("Enemy/enemy1");
                 break;
         }
    
@@ -156,6 +175,7 @@ public class EnemyBaseState : State
         await Task.Delay(3000);
         //reset the player texture
         playerTexture = Globals.Content.Load<Texture2D>("Player/playerIdle");
+        enemyTexture = Globals.Content.Load<Texture2D>("Enemy/enemy1");
     }
 
 
